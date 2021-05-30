8ffc125ab5f899550344fdafc1f41d3bab8f217b
Alex Barnes <AlexBarnes86@gmail.com>
2021-05-30 11:08:12 -0500

    Leetcode Find Numbers with Even Number of Digits

---
+++ src/main/java/com/toastedbits/leetcode/FindNumbersWithEvenNumbersOfDigits.java
@@ -0,0 +1,20 @@
+package com.toastedbits.leetcode;
+
+public class FindNumbersWithEvenNumbersOfDigits {
+    private int countDigits(int num) {
+        int ct = 0;
+        while(num != 0) {
+            ct++;
+            num /= 10;
+        }
+        return ct;
+    }
+
+    public int findNumbers(int[] nums) {
+        int ct = 0;
+        for(int num : nums) {
+            ct += (countDigits(num) % 2 == 0 ? 1 : 0);
+        }
+        return ct;
+    }
+}
