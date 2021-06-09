e678d2a6dcf7ecd44612b455d1579210415053cc
Alex Barnes <AlexBarnes86@gmail.com>
2021-06-08 21:08:18 -0500

    Leetcode LongestSubstringWithoutRepeatingCharacters Improved from GFG

--- src/main/java/com/toastedbits/leetcode/hashtables/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/toastedbits/leetcode/hashtables/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,28 +1,29 @@
 package com.toastedbits.leetcode.hashtables;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
 
 public class LongestSubstringWithoutRepeatingCharacters {
     public int lengthOfLongestSubstring(String s) {
-        if(s.length() == 0) {
-            return 0;
-        }
+        // Creating a set to store the last positions of occurrence
+        HashMap<Character, Integer> seen = new HashMap<>();
+        int maximum_length = 0;
 
-        Set<Character> set = new HashSet<>();
-        int ct = 0;
-        for(int i = 0; i < s.length(); ++i) {
-            char ch = s.charAt(i);
-            if(set.contains(ch)) {
-                return Math.max(ct, lengthOfLongestSubstring(s.substring(1)));
-            }
-            else {
-                set.add(ch);
-                ct++;
-            }
-        }
+        // starting the inital point of window to index 0
+        int start = 0;
 
-        return ct;
+        for (int end = 0; end < s.length(); end++) {
+            // Checking if we have already seen the element or not
+            if (seen.containsKey(s.charAt(end))) {
+                // If we have seen the number, move the start pointer
+                // to position after the last occurrence
+                start = Math.max(start, seen.get(s.charAt(end)) + 1);
+            }
+
+            // Updating the last seen value of the character
+            seen.put(s.charAt(end), end);
+            maximum_length = Math.max(maximum_length, end - start + 1);
+        }
+        return maximum_length;
     }
 
     public static void main(String[] args) {
