ff31fe75711ed06e833c05c23210f9eaa734d0ce
Alex Barnes <AlexBarnes86@gmail.com>
2021-06-14 17:44:40 -0500

    Leetcode FourSum2

--- src/main/java/com/toastedbits/leetcode/hashtables/FourSumTwo.java
+++ src/main/java/com/toastedbits/leetcode/hashtables/FourSumTwo.java
@@ -5,28 +5,33 @@ import java.util.Map;
 
 public class FourSumTwo {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
-        int ct = 0;
-        Map<Integer, Integer> possible123 = new HashMap<>();
-        for(int i = 0; i < nums1.length; ++i) {
-            for(int j = 0; j < nums2.length; ++j) {
-                for (int k = 0; k < nums3.length; ++k) {
-                    possible123.compute(nums1[i]+nums2[j]+nums3[k], (key, v) -> v == null ? 1 : v+1);
-                }
-            }
-        }
+        final Map<Integer, Integer> possible12 = combinations(nums1, nums2);
+        final Map<Integer, Integer> possible34 = combinations(nums3, nums4);
 
-        for (int l = 0; l < nums1.length; ++l) {
-            if(possible123.containsKey(-nums4[l])) {
-                ct += possible123.get(-nums4[l]);
+        int ct = 0;
+        for(Map.Entry<Integer, Integer> entry : possible12.entrySet()) {
+            if(possible34.containsKey(-entry.getKey())) {
+                ct += entry.getValue() * possible34.get(-entry.getKey());
             }
         }
 
         return ct;
     }
 
+    private Map<Integer, Integer> combinations(int[] ary1, int[] ary2) {
+        Map<Integer, Integer> combos = new HashMap<>();
+        for (int num1 : ary1) {
+            for (int num2 : ary2) {
+                combos.compute(num1 + num2, (key, v) -> v == null ? 1 : v + 1);
+            }
+        }
+        return combos;
+    }
+
     public static void main(String[] args) {
         FourSumTwo solver = new FourSumTwo();
-        System.out.println(solver.fourSumCount(new int[]{1,2},new int[]{-2,-1}, new int[]{-1,2}, new int[]{0,2}) == 2);
-        System.out.println(solver.fourSumCount(new int[]{0},new int[]{0}, new int[]{0}, new int[]{0}) == 1);
+        System.out.println("2: " + solver.fourSumCount(new int[]{1,2},new int[]{-2,-1}, new int[]{-1,2}, new int[]{0,2}));
+        System.out.println("1: " + solver.fourSumCount(new int[]{0},new int[]{0}, new int[]{0}, new int[]{0}));
+        System.out.println("132: " + solver.fourSumCount(new int[]{-1,1,1,1,-1}, new int[]{0,-1,-1,0,1}, new int[]{-1,-1,1,-1,-1}, new int[]{0,1,0,-1,-1}));
     }
 }
