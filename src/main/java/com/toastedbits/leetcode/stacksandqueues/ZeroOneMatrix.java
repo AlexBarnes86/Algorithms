9b6eb54701a2d2a48806db287e903a7b3db7f247
Alex Barnes <AlexBarnes86@gmail.com>
2021-06-22 19:10:10 -0500

    Leetcode ZeroOneMatrix

--- src/main/java/com/toastedbits/leetcode/stacksandqueues/ZeroOneMatrix.java
+++ src/main/java/com/toastedbits/leetcode/stacksandqueues/ZeroOneMatrix.java
@@ -11,48 +11,57 @@ public class ZeroOneMatrix {
         int c;
         public Cell(int r, int c) {
             this.r = r;
-            this.c = c;
+            this.c =c;
         }
     }
 
-    public int zeroDistance(int[][] mat, int r, int c) {
+    void fill(int[][] mat, int val) {
+        for(int r = 0; r < mat.length; ++r) {
+            for(int c = 0; c < mat[r].length; ++c) {
+                mat[r][c] = val;
+            }
+        }
+    }
+
+    public int[][] updateMatrix(int[][] mat) {
+        int[][] result = new int[mat.length][mat[0].length];
+        fill(result, -1);
         Queue<Cell> queue = new LinkedList<>();
-        queue.offer(new Cell(r,c));
+        for(int r = 0; r < mat.length; ++r) {
+            for(int c = 0; c < mat[r].length; ++c) {
+                if(mat[r][c] == 0) {
+                    result[r][c] = 0;
+                    queue.offer(new Cell(r, c));
+                }
+            }
+        }
+
         int depth = 0;
         while(!queue.isEmpty()) {
             int levelSize = queue.size();
             for(int i = 0; i < levelSize; ++i) {
                 Cell cur = queue.poll();
-                if(mat[cur.r][cur.c] == 0) {
-                    return depth;
-                }
-
-                if(cur.r - 1 >= 0) {
+                result[cur.r][cur.c] = depth;
+                if(cur.r - 1 >= 0 && result[cur.r - 1][cur.c] == -1) {
                     queue.offer(new Cell(cur.r - 1, cur.c));
+                    result[cur.r-1][cur.c] = -2;
                 }
-                if(cur.r + 1 < mat.length) {
+                if(cur.r + 1 < mat.length && result[cur.r + 1][cur.c] == -1) {
                     queue.offer(new Cell(cur.r + 1, cur.c));
+                    result[cur.r+1][cur.c] = -2;
                 }
-                if(cur.c - 1 >= 0) {
+                if(cur.c - 1 >= 0 && result[cur.r][cur.c - 1] == -1) {
                     queue.offer(new Cell(cur.r, cur.c - 1));
+                    result[cur.r][cur.c-1] = -2;
                 }
-                if(cur.c + 1 < mat[cur.r].length) {
+                if(cur.c + 1 < mat[cur.r].length && result[cur.r][cur.c + 1] == -1) {
                     queue.offer(new Cell(cur.r, cur.c + 1));
+                    result[cur.r][cur.c+1] = -2;
                 }
             }
             depth++;
         }
 
-        throw new IllegalArgumentException("Must provide a matrix with at least one 0");
-    }
-
-    public int[][] updateMatrix(int[][] mat) {
-        int[][] result = new int[mat.length][mat[0].length];
-        for(int r = 0; r < mat.length; ++r) {
-            for(int c = 0; c < mat[r].length; ++c) {
-                result[r][c] = zeroDistance(mat, r, c);
-            }
-        }
         return result;
     }
 
@@ -62,6 +71,9 @@ public class ZeroOneMatrix {
         System.out.println();
 
         ArrayUtil.println(solver.updateMatrix(new int[][]{{0,0,0},{0,1,0},{1,1,1}}));
+        System.out.println();
+
+        ArrayUtil.println(solver.updateMatrix(new int[][]{{0},{0},{0},{0},{0}}));
         System.out.println();
     }
 }
