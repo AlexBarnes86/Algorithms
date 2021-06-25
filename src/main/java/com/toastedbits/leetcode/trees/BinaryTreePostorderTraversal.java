d00d43def4a5d269a631225147e552aa35bc3882
Alex Barnes <AlexBarnes86@gmail.com>
2021-06-25 18:26:22 -0500

    Leetcode Binary Tree Postorder Traversal

---
+++ src/main/java/com/toastedbits/leetcode/trees/BinaryTreePostorderTraversal.java
@@ -0,0 +1,49 @@
+package com.toastedbits.leetcode.trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePostorderTraversal {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return "{val: " + val + "}";
+        }
+    }
+
+    private List<Integer> postorderTraversal(TreeNode root, List<Integer> list) {
+        if(root.left != null) {
+            postorderTraversal(root.left, list);
+        }
+        if(root.right != null) {
+            postorderTraversal(root.right, list);
+        }
+        list.add(root.val);
+
+        return list;
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if(root == null) {
+            return new ArrayList<>();
+        }
+        return postorderTraversal(root, new ArrayList<>());
+    }
+}
