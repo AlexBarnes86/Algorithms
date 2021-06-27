6ba82955cfc527a673f11e005b0b381cd9b4b228
Alex Barnes <AlexBarnes86@gmail.com>
2021-06-27 14:27:45 -0500

    Leetcode PopulatingNextRightPointersInEachNode

---
+++ src/main/java/com/toastedbits/leetcode/trees/PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,58 @@
+package com.toastedbits.leetcode.trees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+//Also solves PopulatingNextRightPointersInEachNodeTwo
+public class PopulatingNextRightPointersInEachNode {
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    };
+
+    public Node connect(Node root) {
+        if(root == null) {
+            return null;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            List<Node> level = new ArrayList<>();
+
+            for(int i = 0; i < size; ++i) {
+                Node cur = queue.poll();
+                if(i != size - 1) {
+                    cur.next = queue.peek();
+                }
+                level.add(cur);
+                if(cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if(cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+        }
+
+        return root;
+    }
+}
