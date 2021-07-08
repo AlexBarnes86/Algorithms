24b26081d2e347e3abc340f892c11f76f4d46eb4
Alex Barnes <AlexBarnes86@gmail.com>
2021-07-07 19:59:32 -0500

    Leetcode BinarySearchTemplate3

--- src/main/java/com/toastedbits/leetcode/utils/BinarySearchTemplate1.java
+++ src/main/java/com/toastedbits/leetcode/utils/BinarySearchTemplate1.java
@@ -1,6 +1,16 @@
 package com.toastedbits.leetcode.utils;
 
 public class BinarySearchTemplate1 {
+    /**
+     * Template #1 is the most basic and elementary form of Binary Search.
+     * It is the standard Binary Search Template that most high schools or universities use when they first teach students
+     * computer science. Template #1 is used to search for an element or condition which can be determined by accessing a
+     * single index in the array.
+     *
+     * + Most basic and elementary form of Binary Search
+     * + Search Condition can be determined without comparing to the element's neighbors (or use specific elements around it)
+     * + No post-processing required because at each step, you are checking to see if the element has been found. If you reach the end, then you know the element is not found
+     */
     public static int binarySearch(int[] nums, int target){
         if(nums == null || nums.length == 0)
             return -1;
