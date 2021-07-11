99abaeea2f2903eccafa3e0624289a72cbf57faf
Alex Barnes <AlexBarnes86@gmail.com>
2021-07-11 16:53:16 -0500

    Leetcode SortAnArray

---
+++ src/main/java/com/toastedbits/leetcode/recursion/SortAnArray.java
@@ -0,0 +1,10 @@
+package com.toastedbits.leetcode.recursion;
+
+import java.util.Arrays;
+
+public class SortAnArray {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums;
+    }
+}
